daeb9c0515997121dccd4b968253c596e428fd48
NGADHIYA <NGADHIYA@jaguarlandrover.com>
2021-07-28 12:07:18 +0530

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import react from 'react'
+import react, { useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    async function getData() {
+      await axios(
+        'https://rosterbuster.aero/wp-content/uploads/dummy-response.json',
+        {
+          method: 'GET',
+        },
+      )
+        .then((res) => {
+          console.log(res)
+          console.log(res.data)
+        })
+        .catch((error) => {
+          console.log('error')
+          console.log(error)
+        })
+    }
+    getData()
+  }, [])
   return (
     <div>
       <h1>Hello</h1>
