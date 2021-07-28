d94c3ad01a96f86d5e28199fecd16510b11f199d
NGADHIYA <NGADHIYA@jaguarlandrover.com>
2021-07-28 13:37:47 +0530

    added static api data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+//import axios from 'axios'
 import './App.css'
+import { jsonData } from './jsonData'
 
 function App() {
+  const [apidata, setApiData] = useState(jsonData)
   useEffect(() => {
-    async function getData() {
-      await axios(
-        'https://rosterbuster.aero/wp-content/uploads/dummy-response.json',
-        {
-          method: 'GET',
-        },
-      )
-        .then((res) => {
-          console.log(res)
-          console.log(res.data)
-        })
-        .catch((error) => {
-          console.log('error')
-          console.log(error)
-        })
-    }
-    getData()
+    // async function getData() {
+    //   await axios(
+    //     'https://rosterbuster.aero/wp-content/uploads/dummy-response.json',
+    //     {
+    //       method: 'GET',
+    //     },
+    //   )
+    //     .then((res) => {
+    //       setApiData(res.data)
+    //     })
+    //     .catch((error) => {
+    //       console.log(error)
+    //     })
+    // }
+    // getData()
+    console.log(apidata)
   }, [])
   return (
     <div>
